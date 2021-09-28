5fbf773940b6979799a0cea5b3b8ca87cad1ba9e
limardijonathan <limardi.jonathan@gmail.com>
2021-09-28 19:39:12 -0300

    modelo mercadoliebre heroku

--- app.js
+++ app.js
@@ -21,34 +21,6 @@ app.get('/register', (req,res)=>{
 });
 
 
-app.listen(3030, () => 
+app.listen(process.env.PORT || 3030, () => 
 console.log('servidor running')
 );
-
-
-
-
-/*
-
-
-const listar = require("../listartareas");
-const escribirJSON = require("../tareas");
-
-switch (true) {
-    case process.argv[2] === 'listar' :
-        listar();
-        break;
-
-    case process.argv[2] === 'Agregar' :
-        let variable = {"id": 2, "titulo": "Netflix","estado": "En curso"}        
-        escribirJSON(variable);
-        break;    
-
-    case process.argv.length == 2 :
-            console.log("Atencion - Tienes que pasar una accion")
-            break;
-    default:
-        console.log("No entiendo que quieres hacer")
-        break;        
-}
-*/
